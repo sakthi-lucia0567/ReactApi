95fc9b54d26dacb62e9bb00822af5569f6733b70
sakthi_l_u_c_i <sakthivel@gomobi.io>
2022-11-24 21:16:56 +0530

    commit

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 /**
  *  ? Alternative for promises
  * todo : Alternative for promises
- * todo : hello world
+ * todo : hello world 1
  */
 
 // const apiData = fetchData("https://dog.ceo/api/breeds/image/random/50");
